d0bb1a021e100181014bb22d61319a06da315d2e
badhon-cse <2221081073@uttarauniversity.edu.bd>
2024-05-02 12:24:54 +0600

    first

---
+++ Query.sql
@@ -0,0 +1,10 @@
+USE UU
+CREATE TABLE student(
+id INT NOT NULL,
+firstname VARCHAR(66),
+address VARCHAR(66));
+
+INSERT INTO student(id, firstname, address) 
+VALUES (73, 'badhon', 'konabari'),
+(50, 'nuru vyutki', 'konabari')
+
